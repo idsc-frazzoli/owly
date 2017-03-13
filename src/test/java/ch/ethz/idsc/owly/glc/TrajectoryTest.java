734db912708d8209692fe11680bcf0b4ad0e3e08
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-03-13 13:09:15 +0100

    tests for trajectory and rn sperical region

---
+++ src/test/java/ch/ethz/idsc/owly/glc/TrajectoryTest.java
@@ -0,0 +1,28 @@
+package ch.ethz.idsc.owly.glc;
+
+import ch.ethz.idsc.owly.glc.adapter.SimpleTrajectoryRegionQuery;
+import ch.ethz.idsc.owly.glc.adapter.TimeInvariantRegion;
+import ch.ethz.idsc.owly.glc.core.StateTime;
+import ch.ethz.idsc.owly.glc.core.Trajectory;
+import ch.ethz.idsc.owly.glc.core.TrajectoryRegionQuery;
+import ch.ethz.idsc.owly.util.rn.RnSphericalRegion;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensors;
+import junit.framework.TestCase;
+
+public class TrajectoryTest extends TestCase {
+  public void testDisjoint() {
+    TrajectoryRegionQuery goalQuery = //
+        new SimpleTrajectoryRegionQuery(new TimeInvariantRegion( //
+            new RnSphericalRegion(Tensors.vectorLong(10, 5), RealScalar.of(1))));
+    Trajectory trajectory = new Trajectory();
+    trajectory.add(new StateTime(Tensors.vectorLong(0, 5), 0));
+    trajectory.add(new StateTime(Tensors.vectorLong(5, 5), 0));
+    assertEquals(goalQuery.firstMember(trajectory), -1);
+    assertTrue(goalQuery.isDisjoint(trajectory));
+    // ---
+    trajectory.add(new StateTime(Tensors.vectorLong(10, 5), 0));
+    assertEquals(goalQuery.firstMember(trajectory), 2);
+    assertFalse(goalQuery.isDisjoint(trajectory));
+  }
+}
