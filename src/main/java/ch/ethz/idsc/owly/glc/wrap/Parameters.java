6621dd137c79ded629fc4490ec93731e39b75e35
jolo1992 <lojonas@ethz.ch>
2017-05-02 15:38:57 +0200

    first working version of parameter class

--- src/main/java/ch/ethz/idsc/owly/glc/wrap/Parameters.java
+++ src/main/java/ch/ethz/idsc/owly/glc/wrap/Parameters.java
@@ -1,11 +1,13 @@
 // code by jl
 package ch.ethz.idsc.owly.glc.wrap;
 
+import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.ZeroScalar;
+import ch.ethz.idsc.tensor.mat.Inverse;
 import ch.ethz.idsc.tensor.sca.Ceiling;
 import ch.ethz.idsc.tensor.sca.Log;
 import ch.ethz.idsc.tensor.sca.Power;
@@ -15,43 +17,39 @@ public class Parameters {
   // Initial condition
   private Tensors x0;
   // Discretization resolution
-  private int resolution;
+  private final int resolution;
   // State space dimension
   private int stateDim;
   // Input space dimension
   private int controlDim;
   // Maximum iterations
-  private int maxIter;
+  private final int maxIter;
   // Change time coordinate to be appropriate
-  private Scalar timeScale;
+  private final Scalar timeScale;
   // Initial partition size
-  private Scalar partitionScale;
-  // maybe change to tensor
+  private final Tensor partitionScale;
   // Adjust initial depth limit
-  private Scalar depthScale;
+  private final Scalar depthScale;
   // integration step
-  private Scalar dtMax;
+  private final Scalar dtMax;
   // Time between nodes
-  private Scalar expandTime;
-  // 1/domainSize
-  private Tensor eta;
+  private final Scalar expandTime;
 
-  /** @param Resolution, resolution of algorithm
-   * @param timeScale Change time coordinate to be appropriate
-   * @param depthScale Adjust initial depth Limit
-   * @param partitionScale Initial Partition Scale
-   * @param maxIter, maximum iterations */
+  /** @param Resolution: resolution of algorithm
+   * @param timeScale: Change time coordinate to be appropriate
+   * @param depthScale: Adjust initial depth Limit
+   * @param partitionScale: Initial Partition Scale
+   * @param dtMax: Maximum integrationstep size
+   * @param maxIter: maximum iterations */
   public Parameters( //
-      int resolution, Scalar timeScale, Scalar depthScale, Scalar partitionScale, Scalar dtMax) {
+      int resolution, Scalar timeScale, Scalar depthScale, Tensor partitionScale, Scalar dtMax, int maxIter) {
     this.resolution = resolution;
     this.timeScale = timeScale;
     this.depthScale = depthScale;
     this.partitionScale = partitionScale;
-    this.expandTime = timeScale.divide(RealScalar.of(resolution));
     this.dtMax = dtMax;
-  }
-
-  public Parameters() {
+    this.maxIter = maxIter;
+    this.expandTime = timeScale.divide(RealScalar.of(resolution));
   }
 
   /** @return time_scale / Resolution */
@@ -60,51 +58,32 @@ public class Parameters {
   }
 
   /** @return depthScale * R * log(R) */
-  public Scalar getDepthLimit() {
-    return this.depthScale.multiply(RealScalar.of(this.resolution)).multiply(Log.function.apply(RealScalar.of(this.resolution))); // TODO add logarithm
+  public int getDepthLimit() {
+    return depthScale.multiply(RealScalar.of(resolution)).multiply(Log.function.apply(RealScalar.of(resolution))).number().intValue(); // TODO add logarithm
   }
 
   /** @param Lipschitz
-   * @return if (Lipschitz ==0) R * log(R)*log(R)/partitionScale else: R^(1+Lipschitz)/partitionscale */
-  public Scalar getEta(Scalar Lipschitz) {
+   * ETA = 1/domainSize
+   * @return if (Lipschitz ==0) R * log(R)²/partitionScale
+   * @return else: R^(1+Lipschitz)/partitionscale */
+  public Tensor getEta(Scalar Lipschitz) {
     if (Lipschitz.equals(ZeroScalar.get()))
-      return RealScalar.of(this.resolution).multiply(Log.function.apply(RealScalar.of(this.resolution))).multiply(Log.function.apply(RealScalar.of(resolution)))
-          .divide(this.partitionScale);
-    return Power.of(RealScalar.of(this.resolution), RealScalar.ONE.add(Lipschitz)).divide(this.partitionScale);
+      return partitionScale.map(Scalar::invert).multiply(RealScalar.of(resolution).multiply(Power.of(Log.function.apply(RealScalar.of(resolution)), 2)));
+    return partitionScale.map(Scalar::invert).multiply(Power.of(RealScalar.of(resolution), RealScalar.ONE.add(Lipschitz)));
   }
 
   /** @return trajectory size with current expandtime and dtMax */
-  public Scalar getTrajectorySize() {
-    return (Scalar) Ceiling.of(this.expandTime.divide(this.dtMax));
+  public int getTrajectorySize() {
+    Scalar temp = (Scalar) Ceiling.of(expandTime.divide(dtMax));
+    return temp.number().intValue(); // TODO needs to be int
+  }
+
+  public int getmaxIter() {
+    return maxIter;
   }
 
   public int getResolution() {
     return resolution;
-  }
-
-  /** @param resolution: set Resolution of GLC */
-  public void setResolution(int resolution) {
-    this.resolution = resolution;
-  }
-
-  /** @param timeScale: Change time coordinate to be appropriate */
-  public void setTimeScale(Scalar timeScale) {
-    this.timeScale = timeScale;
-  }
-
-  /** @param partitionScale: Initial partition size */
-  public void setPartitionScale(Scalar partitionScale) {
-    this.partitionScale = partitionScale;
-  }
-
-  /** @param depthScale: Adjust initial depth limit */
-  public void setDepthScale(Scalar depthScale) {
-    this.depthScale = depthScale;
-  }
-
-  /** @param dtMax: integration step */
-  public void setDtMax(Scalar dtMax) {
-    this.dtMax = dtMax;
   }
 
   public void printResolution() {
