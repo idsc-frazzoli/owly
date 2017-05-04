fa57df59426468ae4f395902563a6b15577a661c
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-05-04 06:18:25 +0200

    free bounded interval

---
+++ src/main/java/ch/ethz/idsc/owly/math/region/FreeBoundedIntervalRegion.java
@@ -0,0 +1,29 @@
+// code by jph
+package ch.ethz.idsc.owly.math.region;
+
+import ch.ethz.idsc.tensor.RationalScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.sca.Abs;
+
+public class FreeBoundedIntervalRegion extends ImplicitFunctionRegion {
+  public static final Scalar HALF = RationalScalar.of(1, 2);
+  // ---
+  private final int index;
+  private final Scalar semiwidth;
+  private final Scalar center;
+
+  public FreeBoundedIntervalRegion(int index, Scalar lo, Scalar hi) {
+    if (!Scalars.lessThan(lo, hi))
+      throw new RuntimeException();
+    this.index = index;
+    semiwidth = hi.subtract(lo).multiply(HALF);
+    center = hi.add(lo).multiply(HALF);
+  }
+
+  @Override
+  public Scalar evaluate(Tensor x) {
+    return semiwidth.subtract(Abs.function.apply(x.Get(index).subtract(center)));
+  }
+}
