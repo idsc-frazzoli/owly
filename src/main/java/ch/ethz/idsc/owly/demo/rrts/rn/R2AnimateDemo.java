ca95ac48e1e1fcafa1fc756708ad95ec5fec5c54
jolo1992 <lojonas@ethz.ch>
2017-05-18 18:16:01 +0200

    WIP:

---
+++ src/main/java/ch/ethz/idsc/owly/demo/rrts/rn/R2AnimateDemo.java
@@ -0,0 +1,62 @@
+// code by jph
+package ch.ethz.idsc.owly.demo.rrts.rn;
+
+import java.io.File;
+import java.util.Random;
+
+import ch.ethz.idsc.owly.glc.adapter.SimpleTrajectoryRegionQuery;
+import ch.ethz.idsc.owly.gui.Gui;
+import ch.ethz.idsc.owly.gui.OwlyFrame;
+import ch.ethz.idsc.owly.math.region.PolygonRegion;
+import ch.ethz.idsc.owly.math.state.TimeInvariantRegion;
+import ch.ethz.idsc.owly.rrts.adapter.DefaultRrts;
+import ch.ethz.idsc.owly.rrts.adapter.LengthCostFunction;
+import ch.ethz.idsc.owly.rrts.adapter.RrtsNodes;
+import ch.ethz.idsc.owly.rrts.adapter.SampledTransitionRegionQuery;
+import ch.ethz.idsc.owly.rrts.core.Rrts;
+import ch.ethz.idsc.owly.rrts.core.RrtsNode;
+import ch.ethz.idsc.owly.rrts.core.RrtsNodeCollection;
+import ch.ethz.idsc.owly.rrts.core.TransitionRegionQuery;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.io.GifSequenceWriter;
+
+class R2AnimateDemo {
+  public static void main(String[] args) throws Exception {
+    int wid = 7;
+    RnTransitionSpace rnss = new RnTransitionSpace();
+    RrtsNodeCollection nc = new RnNodeCollection(Tensors.vector(0, 0), Tensors.vector(wid, wid));
+    TransitionRegionQuery trq = //
+        new SampledTransitionRegionQuery(new SimpleTrajectoryRegionQuery( //
+            new TimeInvariantRegion( //
+                new PolygonRegion(Tensors.matrix(new Number[][] { //
+                    { 3, 1 }, //
+                    { 4, 1 }, //
+                    { 4, 6 }, //
+                    { 1, 6 }, //
+                    { 1, 3 }, //
+                    { 3, 3 } //
+                })))), RealScalar.of(.1));
+    // ---
+    Rrts rrts = new DefaultRrts(rnss, nc, trq, LengthCostFunction.IDENTITY);
+    RrtsNode root = rrts.insertAsNode(Tensors.vector(0, 0), 5);
+    Random random = new Random();
+    OwlyFrame owlyFrame = Gui.start();
+    owlyFrame.configCoordinateOffset(42, 456);
+    owlyFrame.jFrame.setBounds(100, 100, 500, 500);
+    int frame = 0;
+    while (frame++ < 40 && owlyFrame.jFrame.isVisible()) {
+      for (int c = 0; c < 10; ++c) {
+        Tensor pnt = Tensors.vector( //
+            random.nextDouble() * wid, //
+            random.nextDouble() * wid);
+        rrts.insertAsNode(pnt, 20);
+      }
+      owlyFrame.setRrts(root, trq);
+      Thread.sleep(100);
+    }
+    System.out.println(rrts.rewireCount());
+    RrtsNodes.costConsistency(root, rnss, LengthCostFunction.IDENTITY);
+  }
+}
