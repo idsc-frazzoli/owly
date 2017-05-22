68aa3ea23e7c6d027db7a1c89a84ba7bd23b348f
jolo1992 <lojonas@ethz.ch>
2017-05-22 16:50:56 +0200

    cost functionn dependende on input and heuristic for delta example

--- src/main/java/ch/ethz/idsc/owly/demo/glc/delta/DeltaGoalManager.java
+++ src/main/java/ch/ethz/idsc/owly/demo/glc/delta/DeltaGoalManager.java
@@ -12,7 +12,10 @@ import ch.ethz.idsc.owly.math.state.TimeInvariantRegion;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.ZeroScalar;
+import ch.ethz.idsc.tensor.red.Max;
+import ch.ethz.idsc.tensor.red.Norm;
 
 public class DeltaGoalManager extends SimpleTrajectoryRegionQuery implements CostFunction {
   final Tensor center;
@@ -28,11 +31,21 @@ public class DeltaGoalManager extends SimpleTrajectoryRegionQuery implements Cos
 
   @Override
   public Scalar costIncrement(StateTime from, List<StateTime> trajectory, Flow flow) {
-    return RealScalar.of(trajectory.size());
+//    return RealScalar.of(trajectory.size());
+    Scalar sum = RealScalar.of(0);
+    for (int i = 0 ; i< flow.getU().length(); i++)
+    {
+      sum = sum.add(flow.getU().Get(i));
+    }
+   return sum; 
   }
 
   @Override
   public Scalar minCostToGoal(Tensor x) {
     return ZeroScalar.get();
+//    Tensor cur_xy = x.extract(0, 2);
+//    Scalar dxy = Norm._2.of(cur_xy.subtract(center)).subtract(radius);
+//    // Scalar dangle = PRINCIPAL.apply(cur_angle.subtract(angle)).abs().subtract(angle_delta);
+//    return Max.of(dxy, ZeroScalar.get());
   }
 }
