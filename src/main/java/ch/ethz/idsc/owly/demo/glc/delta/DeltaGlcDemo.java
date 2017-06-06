dc6f004257328d730d7c427500c74730757be678
jolo1992 <lojonas@ethz.ch>
2017-06-06 16:59:35 +0200

    Defaultparameterclass

--- src/main/java/ch/ethz/idsc/owly/demo/glc/delta/DeltaGlcDemo.java
+++ src/main/java/ch/ethz/idsc/owly/demo/glc/delta/DeltaGlcDemo.java
@@ -1,90 +1,35 @@
 // code by ?
 package ch.ethz.idsc.owly.demo.glc.delta;
 
-import java.util.Collection;
+import java.util.List;
 
-import ch.ethz.idsc.owly.demo.util.Images;
-import ch.ethz.idsc.owly.demo.util.Resources;
-import ch.ethz.idsc.owly.glc.adapter.Parameters;
-import ch.ethz.idsc.owly.glc.adapter.SimpleTrajectoryRegionQuery;
-import ch.ethz.idsc.owly.glc.core.DefaultTrajectoryPlanner;
 import ch.ethz.idsc.owly.glc.core.Expand;
 import ch.ethz.idsc.owly.glc.core.TrajectoryPlanner;
 import ch.ethz.idsc.owly.gui.Gui;
 import ch.ethz.idsc.owly.gui.OwlyFrame;
-import ch.ethz.idsc.owly.math.flow.Flow;
-import ch.ethz.idsc.owly.math.flow.RungeKutta45Integrator;
-import ch.ethz.idsc.owly.math.region.ImageRegion;
-import ch.ethz.idsc.owly.math.state.FixedStateIntegrator;
-import ch.ethz.idsc.owly.math.state.StateIntegrator;
-import ch.ethz.idsc.owly.math.state.TimeInvariantRegion;
-import ch.ethz.idsc.owly.math.state.TrajectoryRegionQuery;
+import ch.ethz.idsc.owly.math.state.StateTime;
+import ch.ethz.idsc.owly.math.state.Trajectories;
 import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.RealScalar;
-import ch.ethz.idsc.tensor.Scalar;
-import ch.ethz.idsc.tensor.Tensor;
-import ch.ethz.idsc.tensor.Tensors;
-import ch.ethz.idsc.tensor.io.Import;
 
 class DeltaGlcDemo {
   public static void main(String[] args) throws Exception {
-    
-    
-    RationalScalar resolution = (RationalScalar) RationalScalar.of(12, 1);
-    
-    Scalar timeScale = RealScalar.of(10);
-    Scalar depthScale = RealScalar.of(100);
-    Tensor partitionScale = Tensors.vector(32, 32);
-    Scalar dtMax = RationalScalar.of(1, 6);
-    int maxIter = 2000;
-    
-    Tensor range = Tensors.vector(9, 6.5);
-    ImageGradient ipr = new ImageGradient( //
-        Images.displayOrientation(Import.of(Resources.fileFromRepository("/io/delta_uxy.png")).get(Tensor.ALL, Tensor.ALL, 0)), //
-        range, RealScalar.of(-.5)); // -.25 .5
-    DeltaStateSpaceModel stateSpaceModel = new DeltaStateSpaceModel(ipr);
-    Parameters parameters = new DeltaParameters(resolution, timeScale, depthScale,//
-        partitionScale, dtMax, maxIter, stateSpaceModel.getLipschitz());
-    StateIntegrator stateIntegrator = FixedStateIntegrator.create( //
-        new RungeKutta45Integrator(), parameters.getdtMax(), parameters.getTrajectorySize());
-    Scalar maxInput = RealScalar.of(1);
-    maxInput = ipr.maxNorm();
-    Collection<Flow> controls = DeltaControls.createControls( //
-        stateSpaceModel, maxInput, parameters.getResolution());
-    controls = DeltaControls.createControls( //
-        stateSpaceModel, maxInput, 30);
-    Tensor obstacleImage = Images.displayOrientation(Import.of(Resources.fileFromRepository("/io/delta_free.png")).get(Tensor.ALL, Tensor.ALL, 0)); //
-    TrajectoryRegionQuery obstacleQuery = //
-        new SimpleTrajectoryRegionQuery(new TimeInvariantRegion( //
-            new ImageRegion(obstacleImage, range, true)));
-    ExtDeltaGoalManager deltaGoalManager = new ExtDeltaGoalManager( //
-        Tensors.vector(2.9, 2.4), Tensors.vector(.3, .3), maxInput.add(ipr.maxNorm()));
-    TrajectoryPlanner trajectoryPlanner = new DefaultTrajectoryPlanner( //
-        parameters.getEta(), stateIntegrator, controls, deltaGoalManager, deltaGoalManager, obstacleQuery);
-    trajectoryPlanner.insertRoot(Tensors.vector(8.8, 0.5));
-    
-    
-    
-//    TrajectoryPlanner trajectoryPlanner = DeltaGlcHelper.createDefault(RealScalar.of(-0.5), resolution);
+    RationalScalar resolution = (RationalScalar) RationalScalar.of(20, 1);
+    TrajectoryPlanner trajectoryPlanner = DeltaHelper.createGlc(RealScalar.of(-0.25), resolution);
     OwlyFrame owlyFrame = Gui.start();
     owlyFrame.configCoordinateOffset(33, 416);
     owlyFrame.jFrame.setBounds(100, 100, 620, 475);
-    // TODO build depthlimit in for loop
-    // GifSequenceWriter gsw = GifSequenceWriter.of(UserHome.file("delta_glc.gif"), 250);
+    // TODO build depthlimit in for loops
     while (trajectoryPlanner.getBest() == null && owlyFrame.jFrame.isVisible()) {
       Expand.maxSteps(trajectoryPlanner, 30);
+      // TODO BUG: finds goal, but nothing is plotted
       owlyFrame.setGlc(trajectoryPlanner);
-      // gsw.append(owlyFrame.offscreen());
+      List<StateTime> trajectory = trajectoryPlanner.getPathFromRootToGoal();
+      Trajectories.print(trajectory);
       Thread.sleep(1);
       if (trajectoryPlanner.getQueue().isEmpty()) {
         break;
       }
     }
-    
-    // int repeatLast = 6;
-    // while (0 < repeatLast--)
-    // gsw.append(owlyFrame.offscreen());
-    // gsw.close();
-    // System.out.println("created gif");
   }
 }
--- src/main/java/ch/ethz/idsc/owly/demo/glc/rn/R2Parameters.java
+++ src/main/java/ch/ethz/idsc/owly/demo/glc/rn/R2Parameters.java
@@ -1,35 +1,36 @@
 // code by jl
 package ch.ethz.idsc.owly.demo.glc.rn;
 
-import ch.ethz.idsc.owly.glc.adapter.Parameters;
+import ch.ethz.idsc.owly.glc.adapter.DefaultParameters;
 import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
-import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.sca.Log;
 import ch.ethz.idsc.tensor.sca.Power;
 
 //TODO JONAS class should extend from DefaultParameters
-public class R2Parameters extends Parameters {
+public class R2Parameters extends DefaultParameters {
   private final Scalar lipschitz;
 
   public R2Parameters( //
       RationalScalar resolution, Scalar timeScale, Scalar depthScale, Tensor partitionScale, Scalar dtMax, int maxIter, //
       Scalar lipschitz) {
-    super(resolution, timeScale, depthScale, partitionScale, dtMax, maxIter);
+    super(resolution, timeScale, depthScale, partitionScale, dtMax, maxIter, lipschitz);
     this.lipschitz = lipschitz;
   }
 
   @Override
-  /** @return if Lipschitz == 0: R*log(R)²/partitionScale
-   * @return else : R²/partitionScale */
-  public Tensor getEta() {
-    if (Scalars.isZero(lipschitz))
-      return getPartitionScale().map(Scalar::invert) //
-          .multiply(RealScalar.of(getResolution()).multiply(Power.of(Log.of(RealScalar.of(getResolution())), 2)));
+  /** @return if Lipschitz == 0: R*log(R)²/partitionScale */
+  protected Tensor EtaLfZero() {
+    return getPartitionScale().map(Scalar::invert) //
+        .multiply(RealScalar.of(getResolution()).multiply(Power.of(Log.of(RealScalar.of(getResolution())), 2)));
+  }
+
+  @Override
+  /** else : R²/partitionScale */
+  protected Tensor EtaLfNonZero(Scalar lipschitz) {
     return getPartitionScale().map(Scalar::invert) //
         .multiply(Power.of(RealScalar.of(getResolution()), RealScalar.ONE.add(lipschitz)));
-    // TODO change to function depending on Lipschitz
   }
 }
