b679c036d8846b9bb825cb92528d174576a733fc
jolo1992 <lojonas@ethz.ch>
2017-06-20 17:54:30 +0200

    TODOs

---
+++ src/test/java/ch/ethz/idsc/owly/demo/se2/Se2GoalManagerExtTest.java
@@ -0,0 +1,18 @@
+// code by jl
+package ch.ethz.idsc.owly.demo.se2;
+
+import junit.framework.TestCase;
+
+public class Se2GoalManagerExtTest extends TestCase {
+  public void testDefault() {
+    // TODO implement
+  }
+
+  public void testMinCurvature() {
+    // TODO implement
+  }
+
+  public void testWrap() {
+    // TODO implement
+  }
+}
