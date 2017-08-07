575b3d19059fd41ac343ecddafd719264005a52b
jolo1992 <lojonas@ethz.ch>
2017-08-07 17:58:56 +0200

    Demo refactor

--- src/main/java/ch/ethz/idsc/owly/demo/rnxt/glc/R2xtEllipsoidGlcDemo.java
+++ src/main/java/ch/ethz/idsc/owly/demo/rnxt/glc/R2xtEllipsoidGlcDemo.java
@@ -5,7 +5,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import ch.ethz.idsc.owly.demo.rn.R2Parameters;
 import ch.ethz.idsc.owly.demo.rn.RnSimpleEllipsoidGoalManager;
+import ch.ethz.idsc.owly.glc.adapter.Parameters;
 import ch.ethz.idsc.owly.glc.adapter.SimpleTrajectoryRegionQuery;
 import ch.ethz.idsc.owly.glc.adapter.StateTimeTrajectories;
 import ch.ethz.idsc.owly.glc.core.Expand;
@@ -25,15 +27,25 @@ import ch.ethz.idsc.owly.math.state.TrajectoryRegionQuery;
 import ch.ethz.idsc.tensor.DoubleScalar;
 import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 
-enum R2xtEllipsoidDemo {
+enum R2xtEllipsoidGlcDemo {
   ;
   public static void main(String[] args) {
-    Tensor partitionScale = Tensors.vector(5, 5, 1);
-    StateIntegrator stateIntegrator = FixedStateIntegrator.create(EulerIntegrator.INSTANCE, RationalScalar.of(1, 8), 4);
-    Collection<Flow> controls = R2xtControls.createRadial(20);
+    RationalScalar resolution = (RationalScalar) RealScalar.of(8);
+    Tensor partitionScale = Tensors.vector(25, 25, 64);
+    Scalar timeScale = RealScalar.of(6);
+    Scalar depthScale = RealScalar.of(100);
+    Scalar dtMax = RationalScalar.of(1, 6);
+    int maxIter = 1000000;
+    Scalar lipschitz = RealScalar.ONE;
+    Parameters parameters = new R2Parameters(resolution, timeScale, depthScale, partitionScale, dtMax, maxIter, lipschitz);
+    System.out.println("1/DomainSize: " + parameters.getEta());
+    StateIntegrator stateIntegrator = FixedStateIntegrator.create(EulerIntegrator.INSTANCE, parameters.getdtMax(), //
+        parameters.getTrajectorySize());
+    Collection<Flow> controls = R2xtControls.createRadial(parameters.getResolutionInt());
     Tensor goal = Tensors.vector(5, 5, 0);
     RnSimpleEllipsoidGoalManager rnGoal = new RnSimpleEllipsoidGoalManager(//
         goal, Tensors.of(RealScalar.of(0.2), RealScalar.of(0.2), DoubleScalar.POSITIVE_INFINITY));
@@ -44,9 +56,9 @@ enum R2xtEllipsoidDemo {
     // ObstacleEllipsoid around goal, which disappears after 10s
     // ---
     TrajectoryPlanner trajectoryPlanner = new StandardTrajectoryPlanner( //
-        partitionScale, stateIntegrator, controls, obstacleQuery, rnGoal);
+        parameters.getEta(), stateIntegrator, controls, obstacleQuery, rnGoal);
     trajectoryPlanner.insertRoot(Tensors.vector(0, 0, 0));
-    int iters = Expand.maxSteps(trajectoryPlanner, 50000);
+    int iters = Expand.maxSteps(trajectoryPlanner, maxIter);
     System.out.println(iters);
     Optional<GlcNode> optional = trajectoryPlanner.getBest();
     if (optional.isPresent()) {
