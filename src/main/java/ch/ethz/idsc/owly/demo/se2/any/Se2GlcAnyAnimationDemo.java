f60b1ce2b0633b45d92f5b849c4d9345e1c2b73d
jolo1992 <lojonas@ethz.ch>
2017-09-05 16:20:02 +0200

    comparedemos

--- src/main/java/ch/ethz/idsc/owly/demo/se2/any/Se2GlcAnyAnimationDemo.java
+++ src/main/java/ch/ethz/idsc/owly/demo/se2/any/Se2GlcAnyAnimationDemo.java
@@ -1,6 +1,7 @@
 // code by jl
 package ch.ethz.idsc.owly.demo.se2.any;
 
+import ch.ethz.idsc.owly.demo.rn.R2ImageRegions;
 import ch.ethz.idsc.owly.demo.util.ImageRegions;
 import ch.ethz.idsc.owly.gui.ani.OwlyAnimationFrame;
 import ch.ethz.idsc.owly.math.region.ImageRegion;
@@ -11,16 +12,18 @@ enum Se2GlcAnyAnimationDemo {
   ;
   public static void main(String[] args) throws Exception {
     OwlyAnimationFrame owlyAnimationFrame = new OwlyAnimationFrame();
-    Tensor root = Tensors.vector(2.5, 0.75, 0);
+    Tensor root = Tensors.vector(7, 6, 1);
     Se2AnyEntity se2AnyEntity = new Se2AnyEntity(root, 12);
     se2AnyEntity.trajectoryPlannerCallback = owlyAnimationFrame.trajectoryPlannerCallback;
     // Region obstacleRegion = new InvertedRegion(EmptyRegion.INSTANCE);
-    ImageRegion obstacleRegion = ImageRegions.loadFromRepository("/io/track0_100.png", Tensors.vector(10, 10), false);
+    ImageRegion imageRegion = ImageRegions.loadFromRepository("/io/track0_100.png", Tensors.vector(10, 10), false);
     // Region obstacleRegion = new R2NoiseRegion(0.8);
-    se2AnyEntity.startLife(obstacleRegion, root); // (trq, root);
+    imageRegion = R2ImageRegions.inside_gtob();
+    
+    se2AnyEntity.startLife(imageRegion, root); // (trq, root);
     owlyAnimationFrame.set(se2AnyEntity);
     owlyAnimationFrame.configCoordinateOffset(50, 700);
-    owlyAnimationFrame.addBackground(obstacleRegion);
+    owlyAnimationFrame.addBackground(imageRegion);
     owlyAnimationFrame.jFrame.setBounds(100, 50, 1200, 800);
     owlyAnimationFrame.jFrame.setVisible(true);
   }
