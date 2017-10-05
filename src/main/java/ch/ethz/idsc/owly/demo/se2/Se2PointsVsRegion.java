ed946f0e7a493cb1b6e8d139328f7d0bd426bbdb
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-10-05 14:55:02 +0200

    se2 utils

--- src/main/java/ch/ethz/idsc/owly/demo/se2/Se2PointsVsRegion.java
+++ src/main/java/ch/ethz/idsc/owly/demo/se2/Se2PointsVsRegion.java
@@ -6,9 +6,7 @@ import ch.ethz.idsc.owly.math.region.Region;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 
-/**
- * 
- */
+/** used in se2 animation demo to check if footprint of vehicle intersects with obstacle region */
 public class Se2PointsVsRegion implements Region {
   private final Tensor points;
   private final Region region;
@@ -22,7 +20,10 @@ public class Se2PointsVsRegion implements Region {
    * @return true if any of the points subject to the given transformation are in region */
   @Override
   public boolean isMember(Tensor tensor) {
-    return points.dot(Se2Utils.toSE2MatrixTranspose(tensor)).stream().anyMatch(region::isMember);
+    Tensor matrix = Se2Utils.toSE2Matrix(tensor);
+    return points.stream() //
+        .map(point -> matrix.dot(point)) //
+        .anyMatch(region::isMember);
   }
 
   public Tensor points() {
