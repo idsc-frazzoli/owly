8ce6e7c144a2604027a18d3bbe5e446cc781f660
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-11-15 22:41:01 +0100

    camera emulator

--- src/main/java/ch/ethz/idsc/owly/demo/rn/RnRaytracer.java
+++ src/main/java/ch/ethz/idsc/owly/demo/rn/RnRaytracer.java
@@ -1,7 +1,6 @@
 // code by jph
 package ch.ethz.idsc.owly.demo.rn;
 
-import java.util.List;
 import java.util.Optional;
 
 import ch.ethz.idsc.owly.math.SingleIntegratorStateSpaceModel;
@@ -9,30 +8,26 @@ import ch.ethz.idsc.owly.math.StateSpaceModels;
 import ch.ethz.idsc.owly.math.flow.EulerIntegrator;
 import ch.ethz.idsc.owly.math.flow.Flow;
 import ch.ethz.idsc.owly.math.state.FixedStateIntegrator;
-import ch.ethz.idsc.owly.math.state.StateIntegrator;
 import ch.ethz.idsc.owly.math.state.StateTime;
 import ch.ethz.idsc.owly.math.state.TrajectoryRegionQuery;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 
-public class Raytracer {
-  private final StateIntegrator stateIntegrator = // TODO magic const
-      FixedStateIntegrator.create(EulerIntegrator.INSTANCE, RealScalar.of(.05), 20 * 10);
+public class RnRaytracer {
+  private final FixedStateIntegrator stateIntegrator = // TODO magic const
+      FixedStateIntegrator.create(EulerIntegrator.INSTANCE, RealScalar.of(.1), 100);
   private final TrajectoryRegionQuery trajectoryRegionQuery;
 
-  public Raytracer(TrajectoryRegionQuery trajectoryRegionQuery) {
+  public RnRaytracer(TrajectoryRegionQuery trajectoryRegionQuery) {
     this.trajectoryRegionQuery = trajectoryRegionQuery;
   }
 
   /** @param trajectoryRegionQuery
-   * @param origin
+   * @param stateTime
    * @param direction
    * @return */
-  public Optional<StateTime> firstMember(Tensor origin, Tensor direction) {
+  public Optional<StateTime> firstMember(StateTime stateTime, Tensor direction) {
     Flow flow = StateSpaceModels.createFlow(SingleIntegratorStateSpaceModel.INSTANCE, direction);
-    // TODO magic const
-    // TODO JAN integrate only until collision
-    List<StateTime> trajectory = stateIntegrator.trajectory(new StateTime(origin, RealScalar.ZERO), flow);
-    return trajectoryRegionQuery.firstMember(trajectory);
+    return stateIntegrator.firstMember(stateTime, flow, trajectoryRegionQuery);
   }
 }
--- src/main/java/ch/ethz/idsc/owly/demo/se2/glc/Se2Entity.java
+++ src/main/java/ch/ethz/idsc/owly/demo/se2/glc/Se2Entity.java
@@ -2,24 +2,29 @@
 package ch.ethz.idsc.owly.demo.se2.glc;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Objects;
 import java.util.Optional;
 
-import ch.ethz.idsc.owly.demo.rn.Raytracer;
+import ch.ethz.idsc.owly.demo.rn.RnRaytracer;
 import ch.ethz.idsc.owly.glc.core.CostFunction;
 import ch.ethz.idsc.owly.gui.GeometricLayer;
 import ch.ethz.idsc.owly.gui.ani.AbstractEntity;
+import ch.ethz.idsc.owly.math.TensorUnaryOperator;
+import ch.ethz.idsc.owly.math.se2.Se2Bijection;
 import ch.ethz.idsc.owly.math.se2.Se2Utils;
 import ch.ethz.idsc.owly.math.state.EpisodeIntegrator;
 import ch.ethz.idsc.owly.math.state.StateTime;
 import ch.ethz.idsc.owly.math.state.TrajectoryRegionQuery;
 import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Array;
@@ -72,20 +77,31 @@ public abstract class Se2Entity extends AbstractEntity {
     }
     if (Objects.nonNull(raytraceQuery)) {
       StateTime stateTime = getStateTimeNow();
-      Raytracer lidarRaytrace = new Raytracer(raytraceQuery);
+      RnRaytracer lidarRaytrace = new RnRaytracer(raytraceQuery);
       Tensor xya = stateTime.state();
-      // geometricLayer.getMouseSe2State();
       graphics.setColor(new Color(255, 0, 0, 32));
-      for (Tensor angle : Subdivide.of(-Math.PI / 2, +Math.PI / 2, 20)) {
+      Tensor range = Array.zeros(129);
+      int index = 0;
+      for (Tensor angle : Subdivide.of(+Math.PI / 2, -Math.PI / 2, range.length() - 1)) {
         Tensor origin = xya.extract(0, 2).add(AngleVector.of(xya.Get(2)).multiply(RealScalar.of(.1)));
-        Optional<StateTime> optional = //
-            lidarRaytrace.firstMember(origin, AngleVector.of(xya.Get(2).add(angle)));
+        Optional<StateTime> optional = lidarRaytrace.firstMember( //
+            new StateTime(origin, RealScalar.ZERO), AngleVector.of(xya.Get(2).add(angle)));
         if (optional.isPresent()) {
-          Tensor collision = optional.get().state();
-          Shape line = geometricLayer.toPath2D(Tensors.of(origin, collision));
-          graphics.draw(line);
+          Scalar tof = optional.get().time().multiply(RealScalar.of(10));
+          range.set(tof, index);
+          if (index % 8 == 0) {
+            Tensor collision = optional.get().state();
+            Shape line = geometricLayer.toPath2D(Tensors.of(origin, collision));
+            graphics.draw(line);
+          }
         }
+        ++index;
       }
+      graphics.setColor(new Color(224, 224, 255, 128));
+      graphics.fillRect(0, 200, range.length(), 100);
+      graphics.setColor(Color.RED);
+      for (index = 0; index < range.length(); ++index)
+        graphics.fillRect(index, 300 - range.Get(index).number().intValue(), 1, 2);
     }
     { // draw mouse
       Color color = new Color(0, 128, 255, 192);
@@ -96,6 +112,38 @@ public abstract class Se2Entity extends AbstractEntity {
       graphics.setColor(color);
       graphics.fill(geometricLayer.toPath2D(shape()));
       geometricLayer.popMatrix();
+    }
+    if (Objects.nonNull(raytraceQuery)) {
+      // CAMERA
+      Tensor xya = geometricLayer.getMouseSe2State();
+      xya = getStateTimeNow().state();
+      Se2Bijection se2Bijection = new Se2Bijection(xya);
+      TensorUnaryOperator forward = se2Bijection.forward();
+      graphics.setColor(new Color(0, 0, 255, 24));
+      final int WIDTH = 64;
+      BufferedImage bufferedImage = new BufferedImage(WIDTH, WIDTH, BufferedImage.TYPE_INT_ARGB);
+      Graphics g = bufferedImage.getGraphics();
+      g.setColor(Color.DARK_GRAY);
+      g.fillRect(0, 0, WIDTH, WIDTH);
+      g.setColor(new Color(192, 255, 192));
+      for (int x = 0; x < bufferedImage.getWidth(); ++x) {
+        double xn = x / (double) WIDTH;
+        double dist = 0.6 + 1.5 * xn + xn * xn;
+        for (int y = 0; y < bufferedImage.getHeight(); ++y) {
+          Tensor probe = forward.apply(Tensors.vector(dist, (y - WIDTH / 2) * 0.02 * dist));
+          if (raytraceQuery.isMember(new StateTime(probe, RealScalar.ZERO))) { // TODO
+            // Point2D point2D = geometricLayer.toPoint2D(probe);
+            // graphics.fillRect((int) point2D.getX(), (int) point2D.getY(), 2, 2);
+          } else {
+            g.fillRect(WIDTH - y - 1, WIDTH - x - 1, 1, 1);
+          }
+        }
+      }
+      int SCREEN = WIDTH * 2;
+      int OFFSET = 20;
+      graphics.setColor(Color.GREEN);
+      graphics.drawRect(0, OFFSET, SCREEN + 1, SCREEN + 1);
+      graphics.drawImage(bufferedImage, 1, OFFSET + 1, SCREEN, SCREEN, null);
     }
   }
 }
