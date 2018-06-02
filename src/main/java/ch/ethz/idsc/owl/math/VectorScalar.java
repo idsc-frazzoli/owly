5b31cb81cc8f5a4eba123f67e46a344133d5be43
ynager <ynager@student.ethz.ch>
2018-06-02 12:57:18 +0200

    added some interfaces to VectorScalar

--- src/main/java/ch/ethz/idsc/owl/math/VectorScalar.java
+++ src/main/java/ch/ethz/idsc/owl/math/VectorScalar.java
@@ -1,11 +1,25 @@
+// code by ynager
 package ch.ethz.idsc.owl.math;
 
+import java.io.Serializable;
+import java.math.MathContext;
+
 import ch.ethz.idsc.tensor.AbstractScalar;
+import ch.ethz.idsc.tensor.DoubleScalar;
+import ch.ethz.idsc.tensor.ExactScalarQ;
+import ch.ethz.idsc.tensor.MachineNumberQ;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.TensorRuntimeException;
+import ch.ethz.idsc.tensor.sca.Chop;
+import ch.ethz.idsc.tensor.sca.ChopInterface;
+import ch.ethz.idsc.tensor.sca.ExactScalarQInterface;
+import ch.ethz.idsc.tensor.sca.MachineNumberQInterface;
+import ch.ethz.idsc.tensor.sca.N;
+import ch.ethz.idsc.tensor.sca.NInterface;
 
-public class VectorScalar extends AbstractScalar {
+public class VectorScalar extends AbstractScalar implements //
+    ChopInterface, MachineNumberQInterface, NInterface, ExactScalarQInterface, Serializable {
   public static Scalar of(Tensor vector) {
     return new VectorScalar(vector.copy());
   }
@@ -16,50 +30,54 @@ public class VectorScalar extends AbstractScalar {
     this.vector = vector;
   }
 
-  @Override
+  public Tensor vector() {
+    return vector.copy();
+  }
+
+  @Override // from Scalar
   public Scalar multiply(Scalar scalar) {
-    return new VectorScalar(vector.multiply(scalar));
+    return of(vector.multiply(scalar));
   }
 
-  @Override
+  @Override // from Scalar
   public Scalar negate() {
-    return new VectorScalar(vector.negate());
+    return of(vector.negate());
   }
 
-  @Override
+  @Override // from Scalar
   public Scalar reciprocal() {
-    return new VectorScalar(vector.map(Scalar::reciprocal));
+    throw TensorRuntimeException.of(this);
   }
 
-  @Override
+  @Override // from Scalar
   public Scalar abs() {
-    return new VectorScalar(vector.map(Scalar::abs));
+    return of(vector.map(Scalar::abs));
   }
 
-  @Override
+  @Override // from Scalar
   public Number number() {
     throw TensorRuntimeException.of(this);
   }
 
-  @Override
+  @Override // from Scalar
   public Scalar zero() {
-    return new VectorScalar(vector.map(Scalar::zero));
+    return of(vector.map(Scalar::zero));
   }
 
-  @Override
+  @Override // from Scalar
   protected Scalar plus(Scalar scalar) {
     if (scalar instanceof VectorScalar) {
-      return new VectorScalar(((VectorScalar) scalar).vector.add(vector));
+      return of(((VectorScalar) scalar).vector.add(vector));
     }
     throw TensorRuntimeException.of(this, scalar);
   }
 
-  @Override
+  @Override // from Scalar
   public int hashCode() {
     return vector.hashCode();
   }
 
-  @Override
+  @Override // from Scalar
   public boolean equals(Object object) {
     if (object instanceof VectorScalar) {
       return ((VectorScalar) object).vector.equals(vector);
@@ -67,12 +85,35 @@ public class VectorScalar extends AbstractScalar {
     return false;
   }
 
-  @Override
+  @Override // from Scalar
   public String toString() {
     return vector.toString();
   }
 
-  public Tensor vector() {
-    return vector.copy();
+  @Override // from ChopInterface
+  public Scalar chop(Chop chop) {
+    return of(vector.map(v -> chop.apply(v)));
+  }
+
+  // FIXME is this correct?
+  @Override // from MachineNumberQInterface
+  public boolean isMachineNumber() {
+    return !vector.flatten(-1).anyMatch(v -> !MachineNumberQ.of(v));
+  }
+
+  @Override // from NInterface
+  public Scalar n() {
+    return of(vector.map(v -> DoubleScalar.of(v.number().doubleValue())));
+  }
+
+  @Override // from NInterface
+  public Scalar n(MathContext mathContext) {
+    N n = N.in(mathContext.getPrecision());
+    return of(vector.map(v -> n.apply(v)));
+  }
+
+  @Override // from ExactScalarQInterface
+  public boolean isExactScalar() {
+    return ExactScalarQ.all(vector);
   }
 }
--- src/test/java/ch/ethz/idsc/owl/math/VectorScalarTest.java
+++ src/test/java/ch/ethz/idsc/owl/math/VectorScalarTest.java
@@ -1,8 +1,10 @@
 package ch.ethz.idsc.owl.math;
 
+import ch.ethz.idsc.tensor.DoubleScalar;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.sca.Chop;
 import junit.framework.TestCase;
 
 public class VectorScalarTest extends TestCase {
@@ -11,9 +13,18 @@ public class VectorScalarTest extends TestCase {
     assertEquals(a.abs(), VectorScalar.of(Tensors.vector(1, 1, 2)));
     assertEquals(a.add(VectorScalar.of(Tensors.vector(0, 1, 0))), VectorScalar.of(Tensors.vector(1, 0, 2)));
     assertEquals(a.divide(RealScalar.of(2)), VectorScalar.of(Tensors.vector(0.5, -0.5, 1)));
-    assertEquals(a.reciprocal(), VectorScalar.of(Tensors.vector(1, -1, 0.5)));
     assertEquals(a.zero(), VectorScalar.of(Tensors.vector(0, 0, 0)));
     assertEquals(((VectorScalar) a).vector().length(), 3);
+    // ---
+    a = VectorScalar.of(Tensors.vector(0.00001, 0.00005, 0));
+    assertEquals(((VectorScalar) a).chop(Chop._04), VectorScalar.of(Tensors.vector(0, 0, 0)));
+    // ---
+    a = VectorScalar.of(Tensors.of(RealScalar.ONE, DoubleScalar.NEGATIVE_INFINITY));
+    assertFalse(((VectorScalar) a).isMachineNumber());
+    assertFalse(((VectorScalar) a).isExactScalar());
+    a = VectorScalar.of(Tensors.of(RealScalar.ONE, DoubleScalar.ONE));
+    //assertTrue(((VectorScalar) a).isMachineNumber()); //FIXME
+    assertTrue(((VectorScalar) a).isExactScalar());
   }
 
   public void testFail() {
